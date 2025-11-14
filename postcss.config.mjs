75a3f45ae164a0440b9610442e4edf8b3b9cd411
Martin Saposnic <martinsaposnic@gmail.com>
2025-11-14 17:03:18 -0300

    w

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
+  plugins: [
+    [
+      "@tailwindcss/postcss",
+      {
+        optimize: false,
+      },
+    ],
+  ],
+}
